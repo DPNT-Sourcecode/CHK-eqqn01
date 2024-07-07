d916fa3ee81444c7aa6c496f69f111e9136ffb6f
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 16:10:40 +0000

    Sun Jul 07 16:10:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,9 @@ def checkout(skus):
     skusChars = sorted(list(skus.upper()))
     counts = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
     for s in skusChars:
-        counts[s] += 1
+        if s in ['A','B','C','D']:
+            counts[s] += 1
     return 50 * counts['A'] % 3 + 130 * counts['A'] // 3 + 30 * counts['B'] % 2 + 45 * counts['B'] // 2 + 20 * counts['C'] + 15 * counts['D']
+
 
 
