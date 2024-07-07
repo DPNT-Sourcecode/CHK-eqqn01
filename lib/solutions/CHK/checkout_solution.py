6b4ed51c71b160fdb3e56ceaeb0a64035db304b5
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 17:13:24 +0000

    Sun Jul 07 17:13:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,8 @@ def checkout(skus):
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     counts = {(x, 0) for x in alpha}
     prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G': 20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}
+    freeItems = {'E': [(2, 'B')], 'N': [(3, 'M')], 'R': [(3, 'Q')]}
+    modRules = {'A': [(5, 200), (3, 130)], 'B': [(2, 45)], 'F': [(3, 20)], 'H': [(10, 80), (4, 45)], 'K': [(2, 150)], 'P': [(5, 200)], 'Q': [(3, 80)], 'U': [(4, 60)], 'V': [(3, 130), (2, 90)]}
 
     for s in skusChars:
         if s in alpha:
@@ -14,24 +16,20 @@ def checkout(skus):
 
     result = 0
 
-    if counts['E'] > 1:
-        if counts['E'] // 2 >= counts['B']:
-            counts['B'] = 0
-        else:
-            counts['B'] -= counts['E'] // 2
-    
-    if counts['A'] // 5 > 0:
-        result = 200 * (counts['A'] // 5)
-        counts['A'] %= 5
-    
-    result += 50 * (counts['A'] % 3) + 130 * (counts['A'] // 3)
-    result += 30 * (counts['B'] % 2) + 45 * (counts['B'] // 2)
-    result += 20 * counts['C']
-    result += 15 * counts['D']
-    result += 40 * counts['E']
-    result += 10 * (counts['F'] % 3) + 20 * (counts['F'] // 3)
+    for s, v in freeItems:
+        if counts[s] > 1:
+            if counts[s] // v[0] >= counts[v[1]]:
+                counts[v[1]] = 0
+            else:
+                counts[v[0]] -= counts[s] // v[0]
+
+    for s, v in modRules:
+        if counts[s] // v[0] > 0:
+            result = v[1] * (counts[s] // v[0])
+            counts[s] %= v[0]
 
     for s in alpha:
         result += prices[s] * counts[s]
     
     return result
+
