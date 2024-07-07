28860f8a6221f0bc8796febb2eb91326fc138468
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 17:20:30 +0000

    Sun Jul 07 17:20:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,7 @@
 # skus = unicode string
 def checkout(skus):
     skusChars = sorted(list(skus))
-    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alpha = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     counts = {(x, 0) for x in alpha}
     prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G': 20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}
     freeItems = {'E': [[2, 'B']], 'N': [[3, 'M']], 'R': [[3, 'Q']]}
@@ -37,6 +37,7 @@ def checkout(skus):
         result += prices[s] * counts[s]
     
     return result
+
 
 
 
