194884a19f7ee545692965fb7e85831d957f2a43
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 17:31:16 +0000

    Sun Jul 07 17:31:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,9 +7,6 @@ def checkout(skus):
     prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G': 20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}
     freeItems = {'E': [[2, 'B']], 'N': [[3, 'M']], 'R': [[3, 'Q']]}
     modRules = {'A': [[5, 200], [3, 130]], 'B': [[2, 45]], 'F': [[3, 20]], 'H': [[10, 80], [4, 45]], 'K': [[2, 150]], 'P': [[5, 200]], 'Q': [[3, 80]], 'U': [[4, 120]], 'V': [[3, 130], [2, 90]]}
-
-    if len(skusChars) == 0:
-        return -1
     
     for s in skusChars:
         if s in alpha:
@@ -30,7 +27,7 @@ def checkout(skus):
     for s, u in modRules.items():
         for v in u:
             if counts[s] // v[0] > 0:
-                result = v[1] * (counts[s] // v[0])
+                result += v[1] * (counts[s] // v[0])
                 counts[s] %= v[0]
 
     for s in alpha:
