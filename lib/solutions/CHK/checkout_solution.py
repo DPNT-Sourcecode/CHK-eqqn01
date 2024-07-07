9e495887bf4fead303d5d67eb607a4511a16caee
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 16:20:53 +0000

    Sun Jul 07 16:20:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,9 +9,3 @@ def checkout(skus):
         else:
             return -1
     return 50 * (counts['A'] % 3) + 130 * (counts['A'] // 3) + 30 * (counts['B'] % 2) + 45 * (counts['B'] // 2) + 20 * counts['C'] + 15 * counts['D']
-
-
-
-
-
-
