03d508602a5c12859bf716921e2151f16b7a7cf4
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 15:47:04 +0000

    Sun Jul 07 15:47:04 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if isinstance(x, int) and isinstance(y, int):
+        if 1 <= x <= 100 and 1 <= y <= 100:
+            return x + y
+        raise ValueError("expecting values between 1 and 100 for x and y")
+    raise TypeError("expecting integer values for x and y")
+
