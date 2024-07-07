c7232708b79c5ca5f26f4106c10da17e61820528
sbx_user1051 <sbx_user1051@169.254.96.13>
2024-07-07 17:24:19 +0000

    Sun Jul 07 17:24:19 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@
 def checkout(skus):
     skusChars = sorted(list(skus))
     alpha = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    counts = {x:0 for x in alpha}
+    counts = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G': 0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
     prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G': 20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}
     freeItems = {'E': [[2, 'B']], 'N': [[3, 'M']], 'R': [[3, 'Q']]}
     modRules = {'A': [[5, 200], [3, 130]], 'B': [[2, 45]], 'F': [[3, 20]], 'H': [[10, 80], [4, 45]], 'K': [[2, 150]], 'P': [[5, 200]], 'Q': [[3, 80]], 'U': [[4, 120]], 'V': [[3, 130], [2, 90]]}
@@ -37,9 +37,3 @@ def checkout(skus):
         result += prices[s] * counts[s]
     
     return result
-
-
-
-
-
-
